ffc9370f2f55dbc66af6ef4a4d314477e291b7ad
chengtaow <c_t_watson@126.com>
2020-08-16 14:17:44 +0800

    Create OpenGL Initialize & Draw Commands

--- SeaTeaDisplay/MainWindow.xaml.cs
+++ SeaTeaDisplay/MainWindow.xaml.cs
@@ -13,9 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using SharpGL;
 using SharpGL.WPF;
-using SharpGL.SceneGraph.Primitives;
 
 
 namespace SeaTeaDisplay
@@ -25,10 +23,12 @@ namespace SeaTeaDisplay
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowModel mainWindowModel;
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowModel();
+            mainWindowModel = new MainWindowModel();
+            this.DataContext = mainWindowModel;
         }
 
         /// <summary>
@@ -38,25 +38,8 @@ namespace SeaTeaDisplay
         /// <param name="args">The <see cref="OpenGLRoutedEventArgs"/> instance containing the event data.</param>
         private void OpenGLControl_OpenGLDraw(object sender, OpenGLRoutedEventArgs args)
         {
-            OpenGL gl = args.OpenGL;
-
-            // Clear The Screen And The Depth Buffer
-            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-
-            // Move Left And Into The Screen
-            gl.LoadIdentity();
-            gl.Translate(0.0f, 0.0f, -6.0f);
-
-
-            gl.Rotate(rotation, 0.0f, 1.0f, 0.0f);
-
-            Teapot tp = new Teapot();
-            tp.Draw(gl, 14, 1, OpenGL.GL_FILL);
-
-            rotation += 3.0f;
+            mainWindowModel.OpenGLDrawCommand.Execute(args.OpenGL);
         }
-
-        float rotation = 0;
 
         /// <summary>
         /// Handles the OpenGLInitialized event of the OpenGLControl control.
@@ -65,28 +48,7 @@ namespace SeaTeaDisplay
         /// <param name="args">The <see cref="OpenGLRoutedEventArgs"/> instance containing the event data.</param>
         private void OpenGLControl_OpenGLInitialized(object sender, OpenGLRoutedEventArgs args)
         {
-            OpenGL gl = args.OpenGL;
-
-            gl.Enable(OpenGL.GL_DEPTH_TEST);
-
-            float[] global_ambient = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
-            float[] light0pos = new float[] { 0.0f, 5.0f, 10.0f, 1.0f };
-            float[] light0ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
-            float[] light0diffuse = new float[] { 0.3f, 0.3f, 0.3f, 1.0f };
-            float[] light0specular = new float[] { 0.8f, 0.8f, 0.8f, 1.0f };
-
-            float[] lmodel_ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
-            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
-
-            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, global_ambient);
-            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, light0pos);
-            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
-            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, light0diffuse);
-            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, light0specular);
-            gl.Enable(OpenGL.GL_LIGHTING);
-            gl.Enable(OpenGL.GL_LIGHT0);
-
-            gl.ShadeModel(OpenGL.GL_SMOOTH);
+            mainWindowModel.OpenGLInitializeCommand.Execute(args.OpenGL);
         }
     }
 }
--- SeaTeaDisplay/MainWindowModel.cs
+++ SeaTeaDisplay/MainWindowModel.cs
@@ -3,15 +3,64 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using SharpGL;
+using SharpGL.WPF;
+using SharpGL.SceneGraph.Primitives;
+
+
 
 namespace SeaTeaDisplay
 {
     class MainWindowModel : WithNotification
     {
+        private OpenGL gl;
         public MainWindowModel()
         {
+            _OpenGLInitializeCommand = new DelegateCommand(OpenGLInitialize, CanExecute);
+            _OpenGLDrawCommand = new DelegateCommand(OpenGLDraw, CanExecute);
             _ImportCommand = new DelegateCommand(ImportPointClouds, CanExecute);
             BottomMessage = "SeaTea Display initialized.";
+        }
+
+        private void OpenGLInitialize(object param)
+        {
+            gl = (OpenGL)param;
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+
+            float[] global_ambient = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
+            float[] light0pos = new float[] { 0.0f, 5.0f, 10.0f, 1.0f };
+            float[] light0ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            float[] light0diffuse = new float[] { 0.3f, 0.3f, 0.3f, 1.0f };
+            float[] light0specular = new float[] { 0.8f, 0.8f, 0.8f, 1.0f };
+
+            float[] lmodel_ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, global_ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, light0pos);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, light0diffuse);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, light0specular);
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_LIGHT0);
+
+            gl.ShadeModel(OpenGL.GL_SMOOTH);
+        }
+
+        private void OpenGLDraw(object param)
+        {
+            // Clear The Screen And The Depth Buffer
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            // Move Left And Into The Screen
+            gl.LoadIdentity();
+            gl.Translate(0.0f, 0.0f, -6.0f);
+
+
+            gl.Rotate(0f, 0.0f, 1.0f, 0.0f);
+
+            Teapot tp = new Teapot();
+            tp.Draw(gl, 14, 1, OpenGL.GL_FILL);
         }
 
         /// <summary>
@@ -30,10 +79,21 @@ namespace SeaTeaDisplay
 
         #region Commands
         private DelegateCommand _ImportCommand;
-
         public ICommand ImportCommand
         {
             get { return _ImportCommand; }
+        }
+
+        private DelegateCommand _OpenGLDrawCommand;
+        public ICommand OpenGLDrawCommand
+        {
+            get { return _OpenGLDrawCommand; }
+        }
+
+        private DelegateCommand _OpenGLInitializeCommand;
+        public ICommand OpenGLInitializeCommand
+        {
+            get { return _OpenGLInitializeCommand; }
         }
         #endregion
 
